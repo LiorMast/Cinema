a759411837131fab3cf7f16dc61aa5a3595a9271
Lior Masturov <liormasturov@gmail.com>
2023-05-23 19:31:13 +0300

    added movie and moviecollection classes

---
+++ eShop/Movie.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShop
+{
+    internal class Movie
+    {
+        private string title;
+        private string director;
+        private int duration;
+        private string genre;
+
+        public Movie(string title, string director, int duration, string genre)
+        {
+            this.title = title;
+            this.director = director;
+            this.duration = duration;
+            this.genre = genre;
+        }
+
+        public string GetTitle()
+        {
+            return title;
+        }
+
+        public void SetTitle(string newTitle)
+        {
+            title = newTitle;
+        }
+
+        public string GetDirector()
+        {
+            return director;
+        }
+
+        public void SetDirector(string newDirector)
+        {
+            director = newDirector;
+        }
+
+        public int GetDuration()
+        {
+            return duration;
+        }
+
+        public void SetDuration(int newDuration)
+        {
+            duration = newDuration;
+        }
+
+        public string GetGenre()
+        {
+            return genre;
+        }
+
+        public void SetGenre(string newGenre)
+        {
+            genre = newGenre;
+        }
+    }
+
+}
---
+++ eShop/MovieCollection.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShop
+{
+    public class MovieCollection
+    {
+        private Movie[] movies;
+        private int currentIndex;
+
+        public enum Genre
+        {
+            Action,
+            Comedy,
+            Drama,
+            Fantasy,
+            Horror,
+            Romance,
+            SciFi,
+            Thriller
+        }
+
+        public MovieCollection()
+        {
+            movies = new Movie[10]; // Initial capacity of 10
+            currentIndex = 0;
+        }
+
+        public void AddMovie(Movie movie)
+        {
+            if (currentIndex == movies.Length)
+            {
+                // Array is full, resize it
+                Array.Resize(ref movies, movies.Length + 10);
+            }
+
+            movies[currentIndex] = movie;
+            currentIndex++;
+        }
+
+        public void DeleteMovie(string movieName)
+        {
+            for (int i = 0; i < currentIndex; i++)
+            {
+                if (movies[i].GetTitle().Equals(movieName, StringComparison.OrdinalIgnoreCase))
+                {
+                    for (int j = i; j < currentIndex - 1; j++)
+                    {
+                        movies[j] = movies[j + 1];
+                    }
+
+                    movies[currentIndex - 1] = null;
+                    currentIndex--;
+                    return;
+                }
+            }
+        }
+
+        public Movie[] GetMoviesByGenre(Genre genre)
+        {
+            List<Movie> moviesByGenre = new List<Movie>();
+
+            foreach (Movie movie in movies)
+            {
+                if (movie != null && movie.GetGenre().Equals(genre.ToString(), StringComparison.OrdinalIgnoreCase))
+                {
+                    moviesByGenre.Add(movie);
+                }
+            }
+
+            return moviesByGenre.ToArray();
+        }
+
+        public Movie SearchMovieByName(string movieName)
+        {
+            foreach (Movie movie in movies)
+            {
+                if (movie != null && movie.GetTitle().ToString().ToLower().Contains(movieName.ToString().ToLower()))
+                {
+                    return movie;
+                }
+            }
+
+            return null;
+        }
+    }
+
+}
